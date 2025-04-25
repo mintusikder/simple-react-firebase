d74a7a37a65fca4a5af96d7ade4e2e14dc560d5c
Mintu Sikder <mintusikder15@gmail.com>
2025-04-25 21:56:17 +0600

    simple firebase auth

---
+++ simple-firebase-auth/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./layout/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children :[
+      {
+        
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
