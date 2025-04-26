6cfd67ec6f7fd7b2c13a6b7b4cf81290d6ab6522
Mintu Sikder <mintusikder15@gmail.com>
2025-04-26 20:30:59 +0600

    added register input value

--- explore-email-password/src/main.jsx
+++ explore-email-password/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./layout/Root.jsx";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Registration from "./components/Registration.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Registration,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
