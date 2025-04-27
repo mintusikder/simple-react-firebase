22e5129dcdbcf901fa2364709e26344acf98cf89
Mintu Sikder <mintusikder15@gmail.com>
2025-04-27 21:34:54 +0600

    added login and register page

--- auth-integration/src/main.jsx
+++ auth-integration/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Main from "./layout/Main";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/REgister";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Main,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "register",
+        Component: Register,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
