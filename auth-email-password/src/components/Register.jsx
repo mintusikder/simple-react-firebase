8721bf45dd2b7e7675a50c3e5f92c2dd8e50d380
Mintu Sikder <mintusikder15@gmail.com>
2025-04-29 16:58:39 +0600

    added login register context

---
+++ auth-email-password/src/components/Register.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Register = () => {
+  const handelRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email,password)
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form onSubmit={handelRegister} className="fieldset">
+              <label className="label">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="input"
+                placeholder="name"
+              />
+              <label className="label">Photo</label>
+              <input
+                type="text"
+                name="photo"
+                className="input"
+                placeholder="photo"
+              />
+              <label className="label">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+              />
+              <label className="label">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="input"
+                placeholder="Password"
+              />
+
+              <button className="btn btn-neutral mt-4">Register</button>
+            </form>
+            <p>
+              Already have account{" "}
+              <Link className="underline text-blue-600" to={"/login"}>
+                Login
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
---
+++ auth-email-password/src/main.jsx
@@ -0,0 +1,49 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Main from "./layout/Main";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import Order from "./components/Order";
+import AuthProvider from "./context/AuthProvider";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Main,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+      {
+        path: "/order",
+        Component: Order,
+      },
+      {
+        path: "/dashboard",
+        Component: Dashboard,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
