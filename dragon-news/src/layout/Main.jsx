00f93e9216c542e99ec3c3d0b4045ac0fd53bdee
Mintu Sikder <mintusikder15@gmail.com>
2025-05-01 12:46:18 +0600

    added latest news

--- dragon-news/src/layout/Main.jsx
+++ dragon-news/src/layout/Main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
 
 const Main = () => {
   return (
     <div>
       <Header></Header>
+      <section className="w-11/12 mx-auto">
+        <LatestNews></LatestNews>
+      </section>
       <main>
         {/* left */}
         <section></section>
